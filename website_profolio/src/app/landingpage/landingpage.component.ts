0296fc45a5c7bb778eefcc03e2902ed400459fe2
NguTrienLuan <tngu0466@student.monash.edu>
2025-10-06 20:03:28 +1100

    modified the nav bar, and added info for the introduction as well as modified the font for the paragraph

--- website_profolio/src/app/landingpage/landingpage.component.ts
+++ website_profolio/src/app/landingpage/landingpage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ExperienceComponent } from '../experience/experience.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [ExperienceComponent, ProjectsComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
