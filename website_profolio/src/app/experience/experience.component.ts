6f4ed80b6f0b134234c3a42dd8e3d911c85365ff
NguTrienLuan <tngu0466@student.monash.edu>
2026-03-26 18:44:34 +1100

    made some changes to the website layout

--- website_profolio/src/app/experience/experience.component.ts
+++ website_profolio/src/app/experience/experience.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
   constructor() {}
+
+  skillGroups = [
+    {
+      title: 'Programming Languages',
+      items: ['JavaScript', 'TypeScript', 'Python', 'Kotlin', 'HTML', 'CSS']
+    },
+    {
+      title: 'Web And Software Development',
+      items: ['Angular', 'Node.js', 'MongoDB', 'Django', 'Full-stack development', 'Software development']
+    },
+    {
+      title: 'Mobile Development',
+      items: ['Android development', 'iOS development', 'Jetpack Compose', 'Mobile app architecture']
+    },
+    {
+      title: 'Working Style',
+      items: ['Teamwork', 'Problem-solving', 'Hardworking', 'Attention to detail', 'Fast learner', 'Customer-focused thinking']
+    }
+  ];
 }
